52f1fec9d8d1d4a829e4690c54316ef96d5dd201
Kelvin888 <35603292+kelvin888@users.noreply.github.com>
2022-12-03 12:26:38 +0100

    chore: create statistics inner layout (#17)

--- src/pages/Statistics/Statistics.tsx
+++ src/pages/Statistics/Statistics.tsx
@@ -1,8 +1,14 @@
 import { Pane } from 'evergreen-ui'
 import { FC } from 'react'
+import style from './statistics/Statistics.module.scss'
 
 const Statistics: FC = () => {
-  return <Pane>Statistics</Pane>
+  return (
+    <Pane className={style.statisticContainer}>
+      <Pane className={style.main}>main</Pane>
+      <Pane className={style.aside}>aside</Pane>
+    </Pane>
+  )
 }
 
 export default Statistics
